90bdc77b81e5493c6f0260b8aca0ea58cf7f90aa
Snehasis Ghosh <snehasisghosh2017@gmail.com>
2024-03-08 15:32:26 +0530

    added permission

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,10 @@ LOG_FILE=/var/log/install_dependencies.log
 
 echo -e "$(date) - Starting install" >> $LOG_FILE
 
-sudo yum install nginx -y 2>&1 >> $LOG_FILE  
+sudo yum install nginx -y
+if [ $? -ne 0 ]; then
+  echo "Install failed" >> /var/log/install_dependencies.log
+fi
+
 
 echo -e "$(date) - Install complete" >> $LOG_FILE
