e309097f2164c3859aa6b2069a59aa87248cf456
Snehasis Ghosh <snehasisghosh2017@gmail.com>
2024-03-08 14:59:50 +0530

    ADD LOGS

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo yum install nginx -y
+LOG_FILE=/var/log/install_dependencies.log
+
+echo -e "$(date) - Starting install" >> $LOG_FILE
+
+sudo yum install nginx -y 2>&1 >> $LOG_FILE  
+
+echo -e "$(date) - Install complete" >> $LOG_FILE
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo systemctl start nginx
+
+LOG_FILE=/var/log/start_server.log
+
+sudo systemctl start nginx >> $LOG_FILE 2>&1
+
+echo -e "$(date) - Start complete" >> $LOG_FILE
+
